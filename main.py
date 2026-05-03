4622e0093666691f98f62804f9e7ca6e8d6ea20e
ramielchik01 <ramiel_0101@icloud.com>
2026-05-03 07:42:23 +0000

    May 3, 2026, 12:42 AM

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 users= {}
-@app.post
-
-
-@app.get("/")
-def home():
-    return {"message": "Hello world"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None, name: str | None = None):
-    return {"item_id": item_id, "q": q, "name": name}
+@app.post("/register")
+def register(username: str, password: str):
+    if username in users:
+        return{"error":"Username already exict"}
+    users[username] = password
+    return{"message":"Registered succesfull"}
+@app.post("/login")
+def login(username: str, password: str):
+    if username not in users:
+        return{"error":"user is not found"}
+    if users[username] != password:
+        return{"message":"password is not correct"}
+    return{"message": "you loged in succesfully"}
