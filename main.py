72af61552dd57bf7c0ec61a563ddf5bb3bca283a
ramielchik01 <ramiel_0101@icloud.com>
2026-05-03 06:37:52 +0000

    May 2, 2026, 11:37 PM

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def home():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, q: str | None = None, name: str | None = None):
+    return {"item_id": item_id, "q": q, "name": name}
